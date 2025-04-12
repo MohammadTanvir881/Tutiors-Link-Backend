ecd339a4dcba8d19c178dc79c1d3be64425edab4
MohammadTanvir881 <tanvirrashid881@gmail.com>
2025-04-12 21:03:51 +0600

    Update teacher and student is done

--- src/module/Student/student.validation.ts
+++ src/module/Student/student.validation.ts
@@ -27,6 +27,26 @@ export const createStudentValidationSchema = z.object({
   }),
 });
 
+const updateStudentValidationSchema = z.object({
+  body: z.object({
+    name: z.string().min(1, "Name is required"),
+    gender: z.enum(["male", "female", "other"]),
+    dateOfBirth: z.string().optional(),
+    contactNo: z.string().min(1, "Contact number is required"),
+    emergencyContactNo: z
+      .string()
+      .min(1, "Emergency contact number is required"),
+    bloodGroup: z
+      .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
+      .optional(),
+    presentAddress: z.string().min(1, "Present address is required"),
+    permanentAddress: z.string().min(1, "Permanent address is required"),
+    profileImg: z.string().url("Profile image must be a valid URL").optional(),
+    coverImg: z.string().url("Cover image must be a valid URL").optional(),
+  }),
+});
+
 export const studentValidation = {
   createStudentValidationSchema,
+  updateStudentValidationSchema,
 };
